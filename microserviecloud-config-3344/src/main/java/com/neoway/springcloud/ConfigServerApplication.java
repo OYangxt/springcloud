ae7d8e0f715543c2fb8ffa601011ca2ba0a596ef
Baron <15619031887@163.com>
2019-12-03 20:18:48 +0800

    config server

---
+++ microserviecloud-config-3344/src/main/java/com/neoway/springcloud/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.neoway.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author 20190712713
+ * @date 2019/12/3 16:48
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class,args);
+    }
+}
