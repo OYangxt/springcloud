429fa772061af5d6428fd2bf6fe6d6832b2defde
Baron <15619031887@163.com>
2019-12-04 10:26:55 +0800

    config client

---
+++ microservicecloud-config-client-3355/src/main/java/com/neoway/springcloud/ConfigClientApplication.java
@@ -0,0 +1,15 @@
+package com.neoway.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 20190712713
+ * @date 2019/12/4 9:57
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class,args);
+    }
+}
---
+++ microservicecloud-config-client-3355/src/main/java/com/neoway/springcloud/controller/ConfigClientConfig.java
@@ -0,0 +1,27 @@
+package com.neoway.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 20190712713
+ * @date 2019/12/4 9:05
+ */
+@RestController
+public class ConfigClientConfig {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${eureka.client.service-url.defaultZone}")
+    private String defaultZone;
+
+    @RequestMapping(value = "/config")
+    public String getConfig(){
+        return serverPort + ": " + applicationName + ": " + defaultZone;
+    }
+}
