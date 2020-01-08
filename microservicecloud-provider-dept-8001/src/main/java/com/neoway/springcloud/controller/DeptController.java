d158248fd1c2214b43adb0ab64955e77be5092e1
Baron <15619031887@163.com>
2020-01-08 16:29:06 +0800

    添加logback-spring.xml

--- microservicecloud-provider-dept-8001/src/main/java/com/neoway/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/neoway/springcloud/controller/DeptController.java
@@ -33,7 +33,7 @@ public class DeptController {
     @ApiOperation("根据Id查询部门信息")
     @ApiImplicitParam(name = "id", value = "部门的id", required = true, dataType = "long")
     @GetMapping("/dept/get/{id}")
-    public HttpResult get(@PathVariable("id") String id){
+    public HttpResult get(@PathVariable(value = "id") String id){
       //@PathVariable注解标识的参数，会拼接到URL中，此处无需判断参数传递；
         try {
             Dept dept=  deptService.findById(id);
